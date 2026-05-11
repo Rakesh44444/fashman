c6b68adf168900abe0ec1f86f48191d74661e39b
Rakesh44444 <113937137+Rakesh44444@users.noreply.github.com>
2026-05-11 11:51:58 +0530

    update the array

--- This_my_git_project/src/array/Reaverse_array.java
+++ This_my_git_project/src/array/Reaverse_array.java
@@ -2,7 +2,7 @@ package array;
 
 public class Reaverse_array {
 	public static void main(String[] args) {
-		 int arr[] = {10, 20, 30, 40, 50};
+		 int arr[] = {10, 20, 30, 40, 50,90,67,98};
 		 for (int i = arr.length - 1; i >= 0; i--) {
 	            System.out.print(arr[i]+" ");
 	        }
